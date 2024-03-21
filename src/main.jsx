3288ac794b9e89e04b3fe559775767f9d628e99e
Pablo Gimenez <pablogimenez.er@gmail.com>
2024-03-20 22:50:05 -0300

    seguimos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1>Recicladores del Paraná</h1>
+    <img src="./src/assets/logo.png" alt="logo" />
+    <h2>en construcción</h2>
   </React.StrictMode>,
 )
