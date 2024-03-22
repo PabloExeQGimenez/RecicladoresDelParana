666ef1e49b882bd9fb2d4418ec50d648ee60de7c
Pablo Gimenez <pablogimenez.er@gmail.com>
2024-03-22 03:30:41 -0300

    construyendo header

--- src/header.jsx
+++ src/header.jsx
@@ -1,22 +1,23 @@
-import "./header.css";
-import logo from "./assets/imagenes/logo.png";
+import logo from './assets/imagenes/logo.png';
+import './header.css';
 
-function Header() {
-    return ( 
-        <header className="header">
-            <div className="logoH1">
-            <img src={logo} alt="logo"  />
-            <h1>Recicladores del Paraná</h1>
-            </div>
-            <nav className="nav">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#nosotros">Nosotros</a></li>
-                    <li><a href="#servicios">Servicios</a></li>
-                    <li><a href="#contacto">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="logoH1">
+          <img src={logo} alt="logo" />
+          <h1>Recicladores del Paraná {1+5}</h1>
+        </div>
+        <nav className="nav">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#nosotros">Nosotros</a></li>
+            <li><a href="#servicios">Servicios</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
 }
-export default Header;
